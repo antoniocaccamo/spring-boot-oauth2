44eadd7d03dcd2077fe29ec131146f6c2b11e133
antonio caccamo <caccamo.antonio@gmail.com>
2019-06-21 17:29:14 +0200

    ADD : docker

---
+++ client/client.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export TOKEN=`curl -s -u curl_client:password -X POST localhost:8081/oauth/token\?grant_type=client_credentials | egrep -o '[a-f0-9-]{20,}'`
+echo "Got token for curl client as : $TOKEN"
+curl -v localhost:8083/product/products -H "Authorization: Bearer $TOKEN"
+
+# for idx in $(seq 1 50); do echo "---- $idx ---"; curl localhost:8083/product -H "Authorization: Bearer $TOKEN"; echo ""; done;
